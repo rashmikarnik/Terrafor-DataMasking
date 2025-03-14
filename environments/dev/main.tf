d91c8584d5c3c845a6b5754febe8be1902b4d7f6
Rashmi Karnik <raskarnik@deloitte.com>
2025-03-13 17:37:10 -0700

    added table_name in main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -16,6 +16,7 @@ module "deploy-dev" {
   source                 = "../../modules/deploy"
   project_id             = "inlaid-goods-451523-i3"
   bigquery_table_name    = "local.bigquery_table_name"
+  table_name             = "local.table_name"
   data_quality_spec_file = "local.data_quality_spec_file"
   environment            = "dev"
 }
