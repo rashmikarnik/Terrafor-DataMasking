7245dde311d6ede0886e86c27a51e91832993f98
Rashmi Karnik <raskarnik@deloitte.com>
2025-03-26 14:56:57 -0700

    some more changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,8 +2,7 @@
 module "deploy-dev" {
   source                 = "../../modules/deploy"
   project_id             = "inlaid-goods-451523-i3"
-  bigquery_table_name    = "local.bigquery_table_name"
-  table_name             = "local.table_name"
-  data_quality_spec_file = "local.data_quality_spec_file"
+  bigquery_table_name    = "var.source_table"
+  table_name             = "var.table_name"
   environment            = "dev"
 }
